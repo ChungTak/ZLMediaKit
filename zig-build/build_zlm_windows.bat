584edf9211a3becb14fde56042abba78ae51f7b4
ChungTak <chung-tak@qq.com>
2025-03-25 19:08:58 +0800

    修复库名连接错误

--- zig-build/build_zlm_windows.bat
+++ zig-build/build_zlm_windows.bat
@@ -4,8 +4,8 @@ cd zig-build\build-windows
 cmake ..\.. -G "Visual Studio 17 2022" -A x64 -DDISABLE_REPORT=ON -DCMAKE_BUILD_TYPE=Release ^
 -DUSE_SOLUTION_FOLDERS=OFF -DENABLE_WEBRTC=ON ^
 -DSRTP_PREFIX="C:\libsrtp" ^
--DSRTP_INCLUDE_DIRS="C:\libsrtp/include" ^
--DSRTP_LIBRARIES="C:\libsrtp/lib/srtp2.dll"
+-DSRTP_INCLUDE_DIRS="C:\libsrtp\include" ^
+-DSRTP_LIBRARIES="C:\libsrtp\lib\srtp2.lib"
 
 
 cmake --build . --target ALL_BUILD  --config Release --parallel
