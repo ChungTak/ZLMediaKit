6183222602660864e2f9d63ac5a9c6563a5ba492
ChungTak <chung-tak@qq.com>
2025-03-25 10:23:07 +0800

    修复发布脚本安卓ndk错误

--- zig-build/build_android_env.sh
+++ zig-build/build_android_env.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export ANDROID_NDK_ROOT=/home/runner/work/ZLMediaKit/ZLMediaKit/android-ndk
+export ANDROID_NDK_HOME="${ANDROID_NDK_HOME:-/home/runner/work/ZLMediaKit/ZLMediaKit/android-ndk}"
 
 HOST_TAG=linux-x86_64
 
 MIN_SDK_VERSION=21
 
-TOOLCHAIN=$ANDROID_NDK_ROOT/toolchains/llvm/prebuilt/$HOST_TAG
+TOOLCHAIN=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/$HOST_TAG
 
 PATH=$TOOLCHAIN/bin:$PATH
 
